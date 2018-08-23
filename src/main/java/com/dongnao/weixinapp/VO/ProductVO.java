e459e271ce5baab13bb4f8b3168db3eb30a0c8e2
huqiurong <910039686@qq.com>
2018-08-23 23:31:40 +0800

    修改VO2

--- src/main/java/com/dongnao/weixinapp/VO/ProductVO.java
+++ src/main/java/com/dongnao/weixinapp/VO/ProductVO.java
@@ -3,11 +3,14 @@ package com.dongnao.weixinapp.VO;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ProductVO {
     @JsonProperty("name")
     private String cateoryName;
     @JsonProperty("type")
     private String catetoryType;
-
+    @JsonProperty("foods")
+    private List<ProductInfoVO> productInfoVOList;
 }
--- src/main/java/com/dongnao/weixinapp/control/BuyerProductController.java
+++ src/main/java/com/dongnao/weixinapp/control/BuyerProductController.java
@@ -1,9 +1,12 @@
 package com.dongnao.weixinapp.control;
 
+import com.dongnao.weixinapp.VO.ProductVO;
 import com.dongnao.weixinapp.VO.ResultVO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/buyer/product")
@@ -13,6 +16,11 @@ public class BuyerProductController {
         ResultVO resultVO = new ResultVO();
         resultVO.setCode(0);
         resultVO.setMsg("返回成功");
+        ProductVO productVO = new ProductVO();
+        productVO.setCateoryName("热销榜");
+        productVO.setCateoryName("1");
+        productVO.setProductInfoVOList(new ArrayList<>());
+        resultVO.setData(productVO);
         return resultVO;
     }
 }
